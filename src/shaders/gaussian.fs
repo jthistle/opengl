a3c28092c97208ef0a5384379b0be7bf965e5cb7
James Thistlewood <jamesthistlewood@gmail.com>
2022-07-28 14:56:33 +0100

    add shaders for later bloom changes

--- src/shaders/gaussian.fs
+++ src/shaders/gaussian.fs
@@ -1,4 +1,6 @@
 #version 330 core
+#define KERNEL_SIZE 5
+
 out vec4 FragColor;
   
 in vec2 TexCoords;
@@ -6,7 +8,7 @@ in vec2 TexCoords;
 uniform sampler2D image;
   
 uniform bool horizontal;
-uniform float weight[5] = float[] (0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216);
+uniform float weight[KERNEL_SIZE] = float[] (0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216);
 
 void main()
 {             
@@ -14,7 +16,7 @@ void main()
     vec3 result = texture(image, TexCoords).rgb * weight[0]; // current fragment's contribution
     if(horizontal)
     {
-        for(int i = 1; i < 5; ++i)
+        for(int i = 1; i < KERNEL_SIZE; ++i)
         {
             result += texture(image, TexCoords + vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
             result += texture(image, TexCoords - vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
@@ -22,7 +24,7 @@ void main()
     }
     else
     {
-        for(int i = 1; i < 5; ++i)
+        for(int i = 1; i < KERNEL_SIZE; ++i)
         {
             result += texture(image, TexCoords + vec2(0.0, tex_offset.y * i)).rgb * weight[i];
             result += texture(image, TexCoords - vec2(0.0, tex_offset.y * i)).rgb * weight[i];
